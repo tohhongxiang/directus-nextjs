b09b89e0e3d481dcb97c710ff7dbd3a5ba5c98b7
tohhongxiang123 <tohhongxiang@gmail.com>
2021-06-22 00:37:28 +0800

    chore: Update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,14 @@ import Link from 'next/link'
 export default function Page({ products = [] }) {
     return (
         <Layout>
-            <section className="flex flex-col justify-center items-center h-96 px-4 py-16 my-16">
+            <section className="flex flex-col justify-center items-center px-4 py-16 my-16">
                 <h1 className="font-bold text-3xl mb-4">Lorem Ipsum</h1>
                 <p className="font-light opacity-80 tracking-wide max-w-2xl text-center mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sint obcaecati dolores in sapiente odio impedit vero nemo? Perferendis odit tempora cum voluptatibus, molestias ipsam exercitationem illum perspiciatis. Accusamus, quod.</p>
                 <Link href="/products"><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">See our Products</button></Link>
             </section>
             <section className="flex flex-col justify-center items-center px-4 py-16 bg-gray-50">
-                <Link href="/products"><h2 className="font-bold text-3xl mb-4 hover:underline cursor-pointer"><a>Featured Products</a></h2></Link>
-                <div className="flex flex-wrap gap-12 justify-center py-12">
+                <Link href="/products"><h2 className="font-bold text-3xl mb-4 hover:underline cursor-pointer text-center"><a>Featured Products</a></h2></Link>
+                <div className="flex flex-wrap gap-12 justify-center items-start py-12">
                     {products.map(product => (
                         <ProductPreview key={product.id} product={product} />
                     ))}
