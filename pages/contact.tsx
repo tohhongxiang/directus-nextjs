0b7521c74d42759cc7b4cdb1b3391914bbbbb8f3
tohhongxiang123 <tohhongxiang@gmail.com>
2021-06-30 06:10:53 +0800

    chore: Update contact page iframe css

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Layout } from '../components'
 
 export default function contact() {
+    const locationQuery = "macritchie reservoir"
     return (
         <Layout title="Contact Us">
             <section className="text-gray-600 body-font relative">
                 <div className="absolute inset-0 bg-gray-300">
-                    <iframe width="100%" height="100%" frameBorder="0" marginHeight={0} marginWidth={0} title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=changi+airport&ie=UTF8&t=&z=14&iwloc=B&output=embed" style={{ filter: 'contrast(1.2) opacity(0.4)' }}></iframe>
+                    <iframe width="100%" height="100%" frameBorder="0" marginHeight={0} marginWidth={0} title="map" scrolling="no" src={`https://maps.google.com/maps?width=100%&height=600&hl=en&q=${locationQuery}&ie=UTF8&t=&z=14&iwloc=B&output=embed`} style={{ filter: 'contrast(1.2) opacity(0.7)' }}></iframe>
                 </div>
                 <div className="container px-5 py-24 mx-auto flex">
                     <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
