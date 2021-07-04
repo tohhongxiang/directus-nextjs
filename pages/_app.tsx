a89852a75ebcdbfe40fd530838a458cd8ffdeccf
Toh Hong Xiang <tohhongxiang@gmail.com>
2021-07-04 22:04:57 +0800

    feat: Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }) {
 				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="description" content="E-commerce store built with NextJS, Directus and SnipCart" />
+				<link rel="icon" href="icons/favicon.svg" sizes="any" type="image/svg+xml"></link>
 			</Head>
 			<Component {...pageProps} />
 		</SnipcartContextProvider>
